6399c2d07822d5db9a6aa4b63b3721c95c032841
Kurisu <mvrobin@hotmail.com>
2016-03-29 19:35:44 -0700

    noob

--- KurisuRiven/KurisuRiven.cs
+++ KurisuRiven/KurisuRiven.cs
@@ -722,7 +722,7 @@ namespace KurisuRiven
                              isteamfightkappa && !wrektAny() || 
                              menubool("w" + target.ChampionName))
                         {
-                            if (player.HealthPercent < 45)
+                            if (player.HealthPercent < 45 || cc == 0 && !q.IsReady())
                             {
                                 w.Cast();
                             }
